c0fed55dd7c7f8ad449aaf4f17b9ddf765929be8
SGraceGrace <143794400+SGraceGrace@users.noreply.github.com>
2024-10-25 20:54:47 +0530

    new project started

---
+++ src/main/java/com/project/holynet/HolynetApplication.java
@@ -0,0 +1,13 @@
+package com.project.holynet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolynetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolynetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/holynet/HolynetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.holynet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolynetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
